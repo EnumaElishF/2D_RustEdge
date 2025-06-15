dd31d574580a5b7726816e818c5b8d3a943d1ebd
EnumaElish <1692380369@qq.com>
2025-06-15 12:30:08 +0800

    22.6_78.修正CropGenerator和AStar地图节点生成

--- Assets/Scripts/Crop/Logic/CropGenerator.cs
+++ Assets/Scripts/Crop/Logic/CropGenerator.cs
@@ -41,6 +41,8 @@ namespace Farm.CropPlant
                 if(tile == null)
                 {
                     tile = new TileDetails();
+                    tile.gridX = cropGridPos.x;
+                    tile.gridY = cropGridPos.y;
                 }
                 tile.daySinceWatered = -1;
                 tile.seedItemID = seedItemID;
